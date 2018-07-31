af3e504f3b21336292ca12936e182d53e42eb87b
jankilighthouse <jankipatel6890@gmail.com>
2018-07-31 03:03:03 +0000

    Adds looping exerecises

---
+++ ingredients.js
@@ -0,0 +1,25 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+/* WHILE LOOP
+var i = 0;
+ while(i < ingredients.length)
+ {
+console.log(ingredients[i]);
+i++;
+}
+*/
+
+/*. FOR LOOP
+for(var i = 0; i < ingredients.length; i++)
+
+{
+  console.log(ingredients[i]);
+}*/
+
+//FOR BACKWARDS DATA
+
+
+for(var i = ingredients.length - 1; i > 0; i--)
+
+{
+  console.log(ingredients[i]);
+}
